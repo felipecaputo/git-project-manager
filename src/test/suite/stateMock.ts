831c48845c283790844dfa44d84c55f551b013d8
Felipe Caputo <felipe.caputo@mercadolivre.com>
2021-11-01 18:33:09 -0300

    fix mock after update

--- src/test/suite/stateMock.ts
+++ src/test/suite/stateMock.ts
@@ -2,8 +2,11 @@ import { stringify } from "querystring";
 
 export default class StateMock {
     data: Map<String, String>;
+    keys: () => readonly string[];
+
     constructor() {
         this.data = new Map<String, String>();
+        this.keys = () => [...this.data.keys()].map(k => '' + k);
     }
     get(key: string, defaultValue = undefined) {
         return this.data.get(key) || defaultValue;
